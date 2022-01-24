532354141847bb7e632d55efead2f38ec1c8bdfc
haoxiaoli <haoxiaoli@baidu.com>
2022-01-24 11:19:04 +0800

    swiper

---
+++ src/components/Home/HomeSwiper.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="homeswiper">
+        <div class="swiper-bg" :style="{backgroundColor: data.bgColor}"></div>
+        <a-carousel arrows :beforeChange="changeFn">
+            <template #prevArrow>
+            <div class="custom-slick-arrow" style="left: 10px; z-index: 1">
+                <left-circle-outlined />
+            </div>
+            </template>
+            <template #nextArrow>
+            <div class="custom-slick-arrow" style="right: 10px">
+                <right-circle-outlined />
+            </div>
+            </template>
+            <div><h3>1</h3></div>
+            <div><h3>2</h3></div>
+            <div><h3>3</h3></div>
+            <div><h3>4</h3></div>
+        </a-carousel>
+    </div>
+</template>
+<script setup>
+import {LeftCircleOutlined, RightCircleOutlined} from '@ant-design/icons-vue';
+import {reactive, defineProps} from 'vue';
+const props = defineProps({banner: Array});
+const data = reactive({
+    bgColor: props.banner[0].bg_color
+});
+function changeFn(from, to) {
+    data.bgColor = props.banner[to].bg_color;
+}
+</script>
+<style lang="less" scoped>
+.swiper-bg {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 575px;
+    background-color: orangered;
+    transform-origin: 0 0;
+    transform: skew(0, -8deg);
+}
+.ant-carousel :deep(.slick-slide) {
+    text-align: center;
+    height: 575px;
+    line-height: 160px;
+    overflow: hidden;
+}
+
+.ant-carousel :deep(.slick-arrow.custom-slick-arrow) {
+    width: 25px;
+    height: 25px;
+    font-size: 25px;
+    color: #fff;
+    background-color: rgba(31, 45, 61, 0.11);
+    opacity: 0.3;
+    z-index: 1;
+}
+.ant-carousel :deep(.custom-slick-arrow:before) {
+    display: none;
+}
+.ant-carousel :deep(.custom-slick-arrow:hover) {
+    opacity: .5;
+}
+
+.ant-carousel :deep(.slick-slide h3) {
+    color: #fff;
+}
+</style>
--- src/components/HomeCom.vue
+++ src/components/HomeCom.vue
@@ -1,15 +1,20 @@
 <template>
-    <h1>首页</h1>
-    <ul>
-        <li v-for="(item, i) in hero">
-            <h3>{{item.category}}</h3>
-        </li>
-    </ul>
+    <div class="home-page">
+        <HomeSwiper :banner="data.banner"/>
+    </div>
 </template>
 <script setup>
 import * as api from '../api/index';
+import HomeSwiper from '../components/Home/HomeSwiper.vue';
 import {reactive} from 'vue';
 let result = await api.getHomepage();
 // 数组变成响应式
-const hero = reactive(result.hero);
+const data = reactive({banner: result.banner});
 </script>
+<style lang="less" scoped>
+.home-page {
+    padding-top: 46px;
+    height: 575px;
+    position: relative;
+}
+</style>
