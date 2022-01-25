6a89cd05f0a2bacbc37ec6c01e8c892e30d2d2b3
haoxiaoli <haoxiaoli@baidu.com>
2022-01-25 10:40:31 +0800

    控制器

--- src/utils/Base3d.js
+++ src/utils/Base3d.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader';
+// 轨道控制器
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 class Base3d {
     constructor(selector) {
         this.container = document.querySelector(selector);
@@ -16,6 +18,8 @@ class Base3d {
         this.initCamera();
         // 初始化渲染器
         this.initRenderer();
+        // 控制器
+        this.initControls();
     }
     initScene() {
         this.scene = new THREE.Scene();
@@ -39,6 +43,10 @@ class Base3d {
         this.renderer.setPixelRatio(window.devicePixelRatio);
         // 渲染的尺寸大小
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        // 色调映射-电影级别
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        // 明暗，数字越大越亮
+        this.renderer.toneMappingExposure = 2;
         this.container.appendChild(this.renderer.domElement);
     }
     setEvnMap(hdr) {
@@ -54,6 +62,10 @@ class Base3d {
     }
     animate() {
         this.renderer.setAnimationLoop(this.render.bind(this));
+    }
+    initControls() {
+        // 摄像机，要渲染的dom
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     }
 };
 export default Base3d;
