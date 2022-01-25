11d588e90b64398a27a98ecac4d4dcc764b59bdc
haoxiaoli <haoxiaoli@baidu.com>
2022-01-25 10:57:49 +0800

    场景上渲染物体

--- src/utils/Base3d.js
+++ src/utils/Base3d.js
@@ -2,11 +2,14 @@ import * as THREE from 'three';
 import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader';
 // 轨道控制器
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+// 模型解析器
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 class Base3d {
     constructor(selector) {
         this.container = document.querySelector(selector);
         this.camera;
         this.scene;
+        this.model;
         this.renderer;
         this.init();
         this.animate();
@@ -20,6 +23,8 @@ class Base3d {
         this.initRenderer();
         // 控制器
         this.initControls();
+        // 添加物体
+        this.addMesh();
     }
     initScene() {
         this.scene = new THREE.Scene();
@@ -66,6 +71,19 @@ class Base3d {
     initControls() {
         // 摄像机，要渲染的dom
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    }
+    setModel(modelName) {
+        return new Promise((resolve, reject) => {
+            const loader = new GLTFLoader().setPath('./files/gltf/');
+            loader.load(modelName, gltf => {
+                this.model = gltf.scene.children[0];
+                this.scene.add(this.model);
+                resolve(this.modelName + '模型添加成功');
+            });
+        });
+    }
+    addMesh() {
+        this.setModel('women.glb');
     }
 };
 export default Base3d;
